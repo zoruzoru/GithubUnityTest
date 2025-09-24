196db8ef344e9c72ccfc9864a8a7b6ff90eac393
Gonzalo <gonzalortizda23@gmail.com>
2025-09-24 20:11:48 +0200

    OtraPrueba

--- Assets/MyAssets/Codigo.cs
+++ Assets/MyAssets/Codigo.cs
@@ -1,17 +1,23 @@
+using System.Threading;
 using System.Xml.Linq;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class Ejercicio : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    float shield = 3;
-    int lifes = 3;
-    bool alive = true;
-    
+
+    int shield;
+    int lifes;
+    bool alive;
+    int damage;
+    int score;
+    float speed;
+    float tiempo;
+    int segundos;
     void Start()
     {
-
+        StartGame();
     }
 
     // Update is called once per frame
@@ -21,22 +27,71 @@ public class Ejercicio : MonoBehaviour
         {
             Impact();
         }
+        
+        if(Input.GetKeyDown(KeyCode.R) == true && alive == true)
+        {
+            Restart();
+        }
+
+        if(Input.GetKeyDown(KeyCode.F) == true && alive == true)
+        {
+            Healing();
+        }
+        
+        if(Input.GetKeyDown(KeyCode.X)  == true && alive == true)
+        {
+            Score();
+        }
+
+        tiempo += Time.deltaTime;
+        if ( tiempo >= 1f)
+        {
+            segundos++;
+            score++;
+        }
     }
 
-    void Movement()
+    void MostrarMensaje(string msg)
     {
-
+        print(msg);
     }
-    
+    void StartGame()
+    {
+             shield = 3;
+             lifes = 4;
+             alive = true;
+             damage = 1;
+             score = 0;
+    }
+    void Healing()
+    {
+        if (lifes < 3)
+        {
+            lifes++;
+            print("Te has curado 1 de vida, ahora tienes " + lifes + " de vida");
+        }
+        else
+        {
+            print("Ya estás al máximo de vida");
+        }
+    }
+    void Restart()
+    {
+        lifes = 4;
+        shield = 3;
+        print("Has reseteado la partida, vuelves a tener todas las estadisticas");
+    }
     void Impact()
     {
-        shield--;
-        //Indicador de pérdida de escudo
+        //codigo feo
+        shield -= damage;
+        //Función para restar escudo y vida
+
         if (shield == 0)
         {
-            print("Has perdido el escudo");
+            print("Has perdido el escudo, puedes curarte cuando lo necesites con F");
         }
-        //Función para restar escudo y vida
+
         if (shield <= 0)
         {
             lifes--;
@@ -46,13 +101,19 @@ public class Ejercicio : MonoBehaviour
         {
             print("Has recibido un impacto, te queda " + shield + " de escudo");
         }
+
         //Función para terminar la partida
         if (lifes <= 0)
         {
             alive = false;
-            print("GAME OVER"); ;
+            print("GAME OVER, tu puntuación ha sido de "+ score +" puntos.");
         }
 
     }
+    void Score()
+    {
+        print("Tu puntuacion es de " + score + " puntos.");
+    }
+
 }
 
